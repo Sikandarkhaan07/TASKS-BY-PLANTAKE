b70a41e30e0e111018088e181584a1edfcc56b36
Sikandar khan <75332358+Sikandarkhaan07@users.noreply.github.com>
2023-04-16 08:55:09 +0500

    mvvm arch project added

---
+++ mvvm_arch/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_arch/utils/routes/routes.dart';
+import 'package:mvvm_arch/utils/routes/routes_name.dart';
+import 'package:mvvm_arch/view_model/auth_view_model.dart';
+import 'package:mvvm_arch/view_model/user_view_model.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UserViewModel(),
+        ),
+      ],
+      child: const MaterialApp(
+        title: 'MVVM',
+        initialRoute: RoutesName.splashScreen,
+        onGenerateRoute: Routes.generateRoute,
+      ),
+    );
+  }
+}
---
+++ mvvm_arch/lib/utils/routes/routes_name.dart
@@ -0,0 +1,13 @@
+class RoutesName {
+  //authentication screen route name
+  static const String loginScreen = 'login_screen';
+
+  //signup screen route name
+  static const String signUpScreen = 'signup_screen';
+
+  //home screen route name
+  static const String homeScreen = 'home_screen';
+
+  //splash screen route name
+  static const String splashScreen = 'splash_screen';
+}
